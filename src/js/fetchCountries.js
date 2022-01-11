3199a1aa90ff7048e99c1c81b4ffab82ac0bfbc6
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2022-01-11 20:33:43 +0200

    changes

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,12 @@
+import Notiflix from 'notiflix';
+
 function fetchCountries(countryName) {
   const BASE_URL = 'https://restcountries.com/v3.1/name/';
   return fetch(`${BASE_URL}${countryName}?fields=name,capital,population,flags,languages`).then(
     response => {
+      if (!response.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
       return response.json();
     },
   );
