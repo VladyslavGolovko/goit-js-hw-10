e5326ba4d75f6c541972d1d990c242258a503e74
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2022-01-11 11:56:55 +0200

    changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}`).then(response => response.json());
+  return fetch(`${BASE_URL}/name/${name}`).then(response => {
+    return response.json();
+  });
 }
 
 export default { fetchCountries };
