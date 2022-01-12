6669c7a5dd97ff6f9c54680e4f309f0875766ad4
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2022-01-12 11:08:58 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 import { getRefs } from './get-refs';
+import countryListTpl from './country-list.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -33,29 +34,7 @@ function toSelectionData(data) {
   }
 }
 
-function renderBigCard(data) {
-  const markUp = data
-    .map(
-      country => `<li>
-          <p>${country.name.official}</p>
-          <p>${country.capital}</p>
-          <p>${country.population}</p>
-          <p>${Object.keys(country.languages)}</p>
-          <img src="${country.flags.svg}" width="500" height="300"/>
-        </li>`,
-    )
-    .join('');
-  refs.listEl.innerHTML = markUp;
-}
-
 function renderSmallCard(data) {
-  const markUp = data
-    .map(
-      country => `<li>
-        <p>${country.name.official}</p>
-        <img src="${country.flags.svg}" width="150" height="100"/>
-      </li>`,
-    )
-    .join('');
+  const markUp = countryListTpl(data);
   refs.listEl.innerHTML = markUp;
 }
