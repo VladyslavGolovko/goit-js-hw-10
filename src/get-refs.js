205fb172e7c5f5d07410df71608dcada9b809d9e
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2022-01-12 13:20:13 +0200

    changes

--- src/get-refs.js
+++ src/get-refs.js
@@ -1,6 +1,6 @@
 export default function getRefs() {
   return {
-    inputEl: document.querySelector('#search-input'),
+    inputEl: document.querySelector('#search-box'),
     listEl: document.querySelector('.country-list'),
     infoEl: document.querySelector('.country-info'),
   };
